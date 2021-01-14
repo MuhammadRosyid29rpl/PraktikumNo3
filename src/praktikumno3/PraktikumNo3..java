4b5f87ff2671f667f7f3bf26904ef1d1cdeddf6f
MuhammadRosyid29rpl <muhammad_rosyid_29rpl@student.smktelkom-mlg.sch.id>
2021-01-14 22:02:58 +0700

    PraktikumNo3

---
+++ src/praktikumno3/PraktikumNo3..java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikumno3;
+
+/**
+ *
+ * @author ASUS TUF A15
+ */
+public class PraktikumNo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+int a;
+        for (a = 0; a < 2; a++) {
+            System.out.println("");
+        } 
+        int b = 4;
+        for (int z = 0; z < 5; z++) {
+            System.out.print("@");
+        }
+        System.out.println("");
+        System.out.println("   @");
+        System.out.println("  @");
+        System.out.println(" @");
+        for (int z = 0; z < 5; z++) {
+            System.out.print("@");
+        }
+        System.out.println("");
+    }
+ 
+}
+                
+            
+            
+        
+    
+    
+
